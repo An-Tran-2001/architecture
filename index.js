85d291301d67613119ba144bc0b75d05625e8151
An-Tran <93371117+An-Tran-2001@users.noreply.github.com>
2022-08-03 11:47:45 +0700

    a
    a

--- index.js
+++ index.js
@@ -11,8 +11,41 @@ let interval = setInterval(() => {
     count++;
     if (count == boxs.length) {
         clearInterval(interval);
+        document.querySelector('.slogan').classList.add('slg-animation');
+        box_button();
     }
-},20);
+},5);
+
 document.addEventListener('scroll',()=>{
   document.querySelector('.nav').classList.toggle('scroll', window.scrollY > 0);
+  document.getElementById('logo').classList.toggle('scroll-logo', window.scrollY > 0);
+  document.getElementById('text-logo').classList.toggle('scroll-text-logo', window.scrollY > 0);
 })
+function box_button() {
+    //  box button
+    var a =50;
+    var c = document.querySelector(".box-button");
+    var h = c.offsetHeight;
+    var w = c.offsetWidth;
+    const list = [[w,0],[w,h+50],[0,h+50],[0,a],[a,0]];
+    console.log(list);
+    var ctx = c.getContext("2d");
+    ctx.beginPath();
+    ctx.moveTo(a,0);
+    list.forEach((value)=>{ctx.lineTo(value[0],value[1]);});
+    ctx.strokeStyle = "white";
+    ctx.lineWidth = 3;
+    ctx.stroke();
+
+    c.addEventListener('onmousemove',()=>{
+        ctx.clearRect(0,0,w,h);
+        ctx.beginPath();
+        ctx.moveTo(a,0);
+        list.forEach((value)=>{ctx.lineTo(value[0],value[1]);});
+        ctx.strokeStyle = "white";
+        ctx.lineWidth = 3;
+        ctx.stroke();
+    });
+
+
+} 
